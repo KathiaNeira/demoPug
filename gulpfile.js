3289503a378b3c79e1be8c0afce6bbf07065b223
kathia <kathia.neira@orbis.com.pe>
2017-01-27 11:02:19 -0500

    feat(pug):configurando pug con gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+
+gulp.task('pug', function(){
+  return gulp.src('./src/pug/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('pug-watch', function(){
+  gulp.watch('./src/pug/*.pug', ['pug'])
+});
+
+gulp.task('default', ['pug']);
